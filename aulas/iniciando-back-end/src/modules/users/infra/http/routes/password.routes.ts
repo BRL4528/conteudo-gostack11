cfa0e1fed98e994d1718fe190c61052f643cd080
BRL4528 <blgc.sgo@hotmail.com>
2020-07-15 06:06:05 -0400

    update reset token :rocket:

---
+++ aulas/iniciando-back-end/src/modules/users/infra/http/routes/password.routes.ts
@@ -0,0 +1,10 @@
+import Router from 'express';
+
+import SessionsController from '../controllers/SessionsController';
+
+const passwordRouter = Router();
+const sessionController = new SessionsController();
+
+passwordRouter.post('/', sessionController.create);
+
+export default passwordRouter;
